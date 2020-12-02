edafe023d6a660e0195a1cd07fe458e352879027
liran <liran_it@163.com>
2020-12-02 22:39:05 +0800

    Week02

---
+++ Week02/main.go
@@ -0,0 +1,47 @@
+//借鉴
+package main
+
+import(
+	"database/sql"
+	"errors"
+	"fmt"
+)
+
+func main(){
+	id := uint(1)
+	user := UserServer{}
+	user.findUserById(id)
+	fmt.Println(user)
+}
+
+
+// Service层
+type UserServer struct{
+	UserDao *UserDao
+}
+
+func (u *UserServer) findUserById(id uint) *sql.Result{
+	user, err := u.UserDao.findUserById(id)
+	if err != nil{
+		fmt.Println("error info %v", err)
+		return nil
+	}
+	return user
+}
+
+//Dao层
+type UserDao struct{
+	Db *sql.DB
+}
+
+func (u *UserDao) findUserById(id uint) (*sql.Result, error){
+	user, err := u.Db.Exec("SELECT * FROM user WHERE id = ?", id)
+	if errors.Is(err, sql.ErrNoRows){
+		return nil, errors.New("findUserById error:" + sql.ErrNoRows.Error())
+	}
+	if err != nil {
+		return nil, errors.Wrap(err, "findUserById error...") // undefined:errors.Wrap
+	}
+
+	return &user, nil
+}
